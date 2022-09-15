c0c144666f621f35671d3bb17f594acf41a08df2
devansh59 <113086973+devansh59@users.noreply.github.com>
2022-09-15 19:20:02 +0530

    Add files via upload

---
+++ type_casting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//Type Casting
+	
+	int a = 'a';
+	cout << a <<endl;     //This will check 'a' value in ASCII Table And Print it.
+	
+	char c = 97;
+	cout << c <<endl;    //This will check 98 value in ASCII Table And Print it.
+	
+	
+	
+}
