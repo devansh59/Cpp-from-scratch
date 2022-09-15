4804901f73caafd3e151e0e7798284a86362d66c
devansh59 <113086973+devansh59@users.noreply.github.com>
2022-09-15 18:59:31 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a = 5;  //Datatype integer
+	cout << a << endl;
+	
+	char ch = 'C';  // Datatype Character
+	cout << ch << endl;
+	
+	bool bl = true; // Datatype boolean( True or False)
+	cout << bl << endl;
+	
+	float f = 1.2;  // Datatype Float 
+	cout<< f << endl;
+	
+	double d = 1.25; // Datatype Double 
+	cout<< d << endl;
+}
+
