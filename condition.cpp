ba6508ea528c823b737557cfe354ba76377ce51d
Devansh Patel <113086973+devansh59@users.noreply.github.com>
2022-09-17 18:59:56 +0530

    Add files via upload

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"Enter First Number:";
+	cin>>a;
+	cout<<"Enter Second Number:";
+	cin>>b;
+	cout<<"A and B is:"<<a<<"&"<<b<<endl;
+	if(a>b){
+		cout<<a << " is greatest";
+	}
+	else{
+		cout<<b << " is gratest";
+	}
+}
