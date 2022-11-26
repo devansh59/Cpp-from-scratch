8ca4950087b3b6945a01cf3b540c9d9a4c69745f
Devansh Patel <113086973+devansh59@users.noreply.github.com>
2022-11-26 10:17:54 +0530

    Add files via upload

---
+++ reverse_array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int reverse(int arr[],int n){
+	int start = 0;
+	int end = n-1;
+	while(start<=end){
+		swap(arr[start],arr[end]);
+		start++;
+		end--;
+	}
+}
+
+int printarray(int arr[],int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i];
+	}
+	cout<<endl;
+}
+
+int main(){
+	
+	int arr[6] = {1,2,3,4,5,6};
+	int brr[5] = {5,4,3,2,1};
+	
+	reverse(arr,6);
+	reverse(brr,5);
+	
+	printarray(arr,6);
+	printarray(brr,5); 
+}
