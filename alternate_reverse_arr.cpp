5cb0335c97f4e6dd6b49c7428406567267a6705a
Devansh Patel <113086973+devansh59@users.noreply.github.com>
2022-12-13 21:31:19 +0530

    Add files via upload

--- alternate_reverse_arr.cpp
+++ alternate_reverse_arr.cpp
@@ -14,14 +14,19 @@ int print(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i];
     }
+    cout<<endl;
 }
 
 int main()
 {
     int arr[6] = {1,2,3,4,5,6};
+    int brr[5] = {1,2,3,4,5};
     
     reverse(arr,6);
-    
+    reverse(brr,5);
+   
     print(arr,6);
+    print(brr,5);
+
 }
 
